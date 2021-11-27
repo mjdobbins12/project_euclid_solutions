fbc5e558a6970dd6e23a095d913a75775c035176
Mark Dobbins <mjdobbins12@gmail.com>
2021-11-27 13:52:27 +0100

    nearly there

--- src/project_euler/largest_prime_factor.clj
+++ src/project_euler/largest_prime_factor.clj
@@ -10,4 +10,20 @@
               #(not= (mod % (first num)) 0)
               (rest num))
              (conj acc (first num))))))
+
+(defn smallest-factor
+  [x]
+  (first (filter #(= (mod x %) 0)
+                 (range 2 (inc (int (Math/floor (Math/sqrt x))))))))
+
+(defn is-prime?
+  [x]
+  (nil? (smallest-factor x)))
+
+(defn largest-prime-factor 
+  [x]
+  (loop [factor x]
+    (if (is-prime? factor)
+      factor
+      (recur (/ factor (smallest-factor x))))))
 ;; 11718
